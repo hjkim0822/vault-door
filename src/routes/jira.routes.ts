64364f53eae5d6600bcb1c129f26957299a920de
김형준 <hjkim0822@gmail.com>
2025-05-28 16:08:29 +0900

    Commits

--- src/routes/jira.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { handleJiraWebhook } from '../controllers/jira.controller';
-
-const router = Router();
-
-// POST /api/jira/event
-router.post('/event', handleJiraWebhook);
-
-export default router;
